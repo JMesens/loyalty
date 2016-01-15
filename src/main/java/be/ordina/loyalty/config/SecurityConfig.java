88674a296e015f6b36b40c71093f6cafe89f5eec
Jeff Mesens <jeff.mesens@gmail.com>
2016-01-15 10:13:04 +0100

    Add spring security

---
+++ src/main/java/be/ordina/loyalty/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package be.ordina.loyalty.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("user").roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("USER","ADMIN");
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
