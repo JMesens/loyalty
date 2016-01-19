c41aa201b6b675b8f12a03b74f7450899f956968
Jeff Mesens <jeff.mesens@gmail.com>
2016-01-19 11:13:04 +0100

    Add a Zuul gateway server

---
+++ gateway-server/src/test/java/be/ordina/loyalty/GatewayServerApplicationTests.java
@@ -0,0 +1,16 @@
+package be.ordina.loyalty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = GatewayServerApplication.class)
+public class GatewayServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
