c82d87d5dcf3d29a55608fc424d06efa9aa5555a
Jeff Mesens <jeff.mesens@gmail.com>
2016-01-27 23:21:13 +0100

    Initial CurieProvider

--- gateway-server/src/main/java/be/ordina/loyalty/GatewayServerApplication.java
+++ gateway-server/src/main/java/be/ordina/loyalty/GatewayServerApplication.java
@@ -4,26 +4,47 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.ResourceSupport;
-import org.springframework.hateoas.mvc.BasicLinkBuilder;
+import org.springframework.hateoas.UriTemplate;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.hal.CurieProvider;
+import org.springframework.hateoas.hal.DefaultCurieProvider;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.springframework.hateoas.mvc.BasicLinkBuilder.linkToCurrentMapping;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 import static org.springframework.http.ResponseEntity.ok;
 
 @SpringBootApplication
 @EnableZuulProxy
 @RestController
 @EnableCircuitBreaker
+@EnableHypermediaSupport(type = EnableHypermediaSupport.HypermediaType.HAL)
 public class GatewayServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+
+    Map<String, UriTemplate> curries = new HashMap<>();
+
+    @Bean
+    public CurieProvider curieProvider() {
+        curries.put("aw", new UriTemplate("awards/docs/{rel}.html"));
+        curries.put("lp", new UriTemplate("loyaltyPrograms/docs/{rel}.html"));
+        curries.put("cu", new UriTemplate("customers/docs/{rel}.html"));
+        curries.put("lo", new UriTemplate("docs/{rel}.html"));
+        final DefaultCurieProvider defaultCurieProvider = new DefaultCurieProvider(curries);
+
+        return defaultCurieProvider;
     }
 
     @RequestMapping("/")
@@ -31,15 +52,14 @@ public class GatewayServerApplication {
 
         ResourceSupport resource = new ResourceSupport();
         String self = linkToCurrentMapping().toString();
-        self = self.replace("://","://admin:admin@");
-        resource.add(new Link(self + "/loyaltyPrograms/loyaltyPoints/{customerId}").withRel("loyaltyPoints"));
-        resource.add(new Link(self + "/loyaltyPrograms/{loyaltyProgramId}").withRel("loyaltyProgram"));
-        resource.add(new Link(self + "/awards/").withRel("awards"));
-        resource.add(new Link(self + "/awards/{awardId}").withRel("award"));
-        resource.add(new Link(self + "/awards/loyaltyProgram/{loyaltyProgramId}").withRel("awardByLoyaltyProgram"));
-        resource.add(new Link(self + "/awards/eligibleAwards/{loyaltyProgramId}/{points}").withRel("eligibleAwards"));
+        self = self.replace("://", "://admin:admin@");
+        resource.add(new Link(self + "/loyaltyPrograms/loyaltyPoints/{customerId}").withRel("lp:loyaltyPoints"));
+        resource.add(new Link(self + "/loyaltyPrograms/{loyaltyProgramId}").withRel("lp:loyaltyProgram"));
+        resource.add(new Link(self + "/awards/").withRel("aw:awards"));
+        resource.add(new Link(self + "/awards/{awardId}").withRel("aw:award"));
+        resource.add(new Link(self + "/awards/loyaltyProgram/{loyaltyProgramId}").withRel("aw:awardByLoyaltyProgram"));
+        resource.add(new Link(self + "/awards/eligibleAwards/{loyaltyProgramId}/{points}").withRel("aw:eligibleAwards"));
         resource.add(new Link(self + "/customers/{customerId}").withRel("customer"));
-        resource.add(new Link("https://hystrix-server.195.13.10.14.xip.io/hystrix/monitor?stream=http%3A%2F%2Fgateway-server.195.13.10.14.xip.io%2Fhystrix.stream"));
 
         return ok(resource);
     }
